931980bf96ab7740b1b182d48e4a1b52ff44d2f6
Rihards Zauls <rihards.zauls@gmail.com>
2024-03-23 17:43:05 +0200

    add response status code to log middleware

--- httpd/middleware.go
+++ httpd/middleware.go
@@ -6,15 +6,30 @@ import (
 	"net/http"
 )
 
+type wrappedWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (w *wrappedWriter) WriteHeader(statusCode int) {
+	w.ResponseWriter.WriteHeader(statusCode)
+	w.statusCode = statusCode
+}
+
 func newLoggerMiddleware(logger *slog.Logger) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			logger.Info(fmt.Sprintf(
-				"%s %s",
-				r.Method,
-				r.URL.String(),
-			))
-			next.ServeHTTP(w, r)
+			wrapped := &wrappedWriter{
+				ResponseWriter: w,
+				statusCode:     http.StatusOK,
+			}
+
+			next.ServeHTTP(wrapped, r)
+
+			logger.Info(
+				fmt.Sprintf("%s %s", r.Method, r.URL.String()),
+				slog.Int("statusCode", wrapped.statusCode),
+			)
 		})
 	}
 }
